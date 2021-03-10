db7dbd504e284dad6378657c8913fa2faa1d910b
Terrance <465446853yan@gmail.com>
2021-03-10 21:57:07 +1100

    Using a middleware(react-redux)

---
+++ WebpackReact05 - redux02 react-redux plugins/src/containers/app.jsx
@@ -0,0 +1,9 @@
+import {connect} from 'react-redux';
+import Counter from '../components/counter';
+import {increment,decrement} from '../redux/actions';
+
+export default connect(
+  state=>({count:state}),
+  {increment,decrement}
+)(Counter)
+
